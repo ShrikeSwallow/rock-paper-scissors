cadda46e1ca7c6a7854f2d6faee8e9d9429e61fb
Maciej Jaskolka <maciej.jaskolka@gmail.com>
2024-10-24 18:18:18 +0100

    Finish RPS GUI

--- index.js
+++ index.js
@@ -9,79 +9,93 @@ const getComputerChoice = () => {
   }
 };
 
-const getHumanChoice = () => {
-  let humanChoice = prompt("Paper! Rock! Scissors! Choose:").toLowerCase();
-  return humanChoice;
+const humanChoice = document.querySelector("#choices");
+const results = document.querySelector("#results");
+const rounds = document.querySelector("#rounds");
+
+humanChoice.addEventListener("click", (event) => {
+  const choice = getHumanChoice(event);
+  console.log(choice);
+  if (choice !== null) {
+    playGame(choice, getComputerChoice());
+  }
+});
+
+const getHumanChoice = (event) => {
+  let target = event.target;
+  switch (target.id) {
+    case "rock":
+      return "rock";
+    case "paper":
+      return "paper";
+    case "scissors":
+      return "scissors";
+    default:
+      return alert("Pick one of the options");
+  }
 };
 
 let humanScore = 0;
 let computerScore = 0;
 
 const playRound = (humanChoice, computerChoice) => {
+  const result = document.createElement("li");
   if (humanChoice === computerChoice) {
-    console.log("You tied! Try again.");
-    return;
+    result.textContent = `You tied! Play next round, current score: ${humanScore} - ${computerScore}`;
+    rounds.appendChild(result);
   }
   switch (humanChoice) {
     case "paper":
       if (computerChoice === "scissors") {
-        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        result.textContent = `You lost this round! ${computerChoice} beats ${humanChoice}`;
+        rounds.appendChild(result);
         computerScore += 1;
         break;
       } else {
-        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        result.textContent = `You won this round! ${humanChoice} beats ${computerChoice}`;
+        rounds.appendChild(result);
         humanScore += 1;
         break;
       }
     case "rock":
       if (computerChoice === "paper") {
-        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        result.textContent = `You lost this round! ${computerChoice} beats ${humanChoice}`;
+        rounds.appendChild(result);
         computerScore += 1;
         break;
       } else {
-        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        result.textContent = `You won this round! ${humanChoice} beats ${computerChoice}`;
+        rounds.appendChild(result);
         humanScore += 1;
         break;
       }
     case "scissors":
       if (computerChoice === "rock") {
-        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        result.textContent = `You lost this round! ${computerChoice} beats ${humanChoice}`;
+        rounds.appendChild(result);
         computerScore += 1;
         break;
       } else {
-        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        result.textContent = `You won this round! ${humanChoice} beats ${computerChoice}`;
+        rounds.appendChild(result);
         humanScore += 1;
         break;
       }
-    default:
-      console.log(
-        `You have to choose from rock, paper or scissors. ${
-          humanChoice[0].toUpperCase() + humanChoice.substring(1).toLowerCase()
-        } is not an option!`
-      );
-      break;
   }
 };
 
-const playGame = () => {
-  let isWinner = false;
-  while (!isWinner) {
-    console.log(`The current score is: Human ${humanScore} - ${computerScore}`);
-    playRound(getHumanChoice(), getComputerChoice());
-    if (humanScore === 3) {
-      isWinner = true;
-      console.log(
-        `Congratulations! You won the game. The final score was 3 - ${computerScore}`
-      );
-    } else if (computerScore === 3) {
-      isWinner = true;
-      console.log(
-        `Sorry, computer got the best of you this time. The final score was: 3 - ${humanScore}`
-      );
-    }
+const playGame = (humanChoice, computerChoice) => {
+  const intro = document.createElement("li");
+  const win = `Congratulations! You won the game. The final score was 3 - ${computerScore}`;
+  const loss = `Sorry, computer got the best of you this time. The final score was: 3 - ${humanScore}`;
+  intro.textContent = `The current score is: Human ${humanScore} - ${computerScore}`;
+  rounds.appendChild(intro);
+  playRound(humanChoice, computerChoice);
+  if (humanScore === 3) {
+    alert(win);
+    window.location.reload();
+  } else if (computerScore === 3) {
+    alert(loss);
+    window.location.reload();
   }
 };
-
-playGame();
-// console.log(getComputerChoice());
-// console.log(getHumanChoice());
